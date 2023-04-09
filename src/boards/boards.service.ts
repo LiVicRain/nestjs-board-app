5d474ee5c264e33bf8ae7ffbfc97d2e03eaf160f
Vic_Rain <tmddntlr@gmail.com>
2023-04-09 23:37:44 +0900

    1.0.0 commit

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Board, BoardStatus } from './model/board.model';
+import { v4 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`해당 아이디를 찾을 수 없습니다 id = ${id}`);
+    }
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoardById(id: string): void {
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
+  }
+}
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../model/board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1; // indexOf에 없는 값이 올경우 -1 을 내보낸다
+  }
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value}는 정확한 status가 아닙니다`);
+    }
+    console.log('metadata', metadata);
+
+    return value;
+  }
+}
